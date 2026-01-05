2188a426ac1b92fa480f774567726dc3a636257d
Antooon32 <robotkartoshka@gmail.com>
2026-01-05 13:34:21 +0200

    fix form 3

--- users/views.py
+++ users/views.py
@@ -1,26 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
-from .forms import MyRegisterForm
-
-# Create your views here.
+from .forms import MyRegisterForm 
 
 def register(request):
-    """Register a new user."""
-    raise Exception("QWERTY")
     if request.method != 'POST':
-        # Показати порожню форму регістрації
         form = MyRegisterForm()
     else:
-        # Опрацювати заповнену форму.
         form = MyRegisterForm(data=request.POST)
-
         if form.is_valid():
             new_user = form.save()
-            # Авторизувати користувача та скерувати його на головну сторінку.
             login(request, new_user)
             return redirect('learning_logs:index')
         
-    # Показати порожню фбо недійсну форму.
     context = {'form': form}
     return render(request, 'registration/register.html', context)
