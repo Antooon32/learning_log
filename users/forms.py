8ef710485d0635a85305d9e992a5fd41a2c9e26d
Antooon32 <robotkartoshka@gmail.com>
2026-01-05 13:42:37 +0200

    fix form 4

--- users/forms.py
+++ users/forms.py
@@ -6,3 +6,14 @@ class MyRegisterForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ("username",)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        allowed_fields = ['username', 'password1', 'password2']
+        
+        all_fields = list(self.fields.keys())
+        
+        for field_name in all_fields:
+            if field_name not in allowed_fields:
+                del self.fields[field_name]
